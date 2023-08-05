4d787074c63c99e8eabc86f2b2f914a8cfc71af9
diogoscastro <diogocas89@gmail.com>
2023-08-05 17:03:35 -0300

    deploy config

--- database/db.js
+++ database/db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose
-    .connect(
-      "mongodb+srv://diogocas89:BNDS83dsc1949@todolist.ihde8uc.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas CONECTADO"))
     .catch((err) => console.log(err));
 };
